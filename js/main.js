868c93af27eef3d04ce4096688302e35e9b7153d
t3nma <brvnocasteleiro@gmail.com>
2021-12-23 00:17:00 +0000

    main.js minor fixes and refactor

--- js/main.js
+++ js/main.js
@@ -1,65 +1,78 @@
+var navbar;
+var navbarHeight;
+var lastScrollTop = 0;
+
+function navbarAutoHide() {
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > navbarHeight / 2) {
+    if (scrollTop < lastScrollTop) {
+      navbar.classList.remove('scrolled-down');
+      navbar.classList.add('scrolled-up');
+    } else {
+      navbar.classList.remove('scrolled-up');
+      navbar.classList.add('scrolled-down');
+    }
+  }
+
+  lastScrollTop = scrollTop;
+}
+
+function brandScrollToTop() {
+  let scrollTop = window.scrollY;
+
+  if (scrollTop > navbarHeight) {
+    window.scrollTo(0,0);
+    return false;
+  }
+}
+
+function isMobile() {
+  try {
+    if(/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)) {
+      return true;
+    };
+  } catch(e) {
+    console.log("Error in isMobile");
+  }
+  return false;
+}
+
+function readyToLink() {
+  if (this.firstElementChild.tagName == 'A' && this.classList.contains('ready-to-link')) {
+    location = this.firstElementChild.href;
+  } else {
+    resetReadyToLink();
+    this.classList.add('ready-to-link');
+    return false;
+  }
+}
+
+function resetReadyToLink() {
+  let ready = document.getElementsByClassName('ready-to-link');
+  if (ready.length) {
+    ready[0].classList.remove('ready-to-link');
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    /* Navbar auto-hide */
-    // fixed-top correction
-    autohide = document.querySelector('.autohide');
-    height = autohide.offsetHeight;
-    document.body.style.paddingTop = height + 'px';
+    /* Navbar autohide and brand scroll to top */
+    navbar = document.querySelector('.autohide');
+    navbarHeight = navbar.offsetHeight;
 
-    // auto-hide scroll listener
-    if(autohide) {
-      var last_scroll_top = 0;
-      window.addEventListener('scroll', function() {
-          let scroll_top = window.scrollY;
-          if (scroll_top > height / 2) {
-            if(scroll_top < last_scroll_top) {
-                autohide.classList.remove('scrolled-down');
-                autohide.classList.add('scrolled-up');
-            } else {
-                autohide.classList.remove('scrolled-up');
-                autohide.classList.add('scrolled-down');
-            }
-          }
-          last_scroll_top = scroll_top;
-      }); 
-    }
+    // navbar .fixed-top correction
+    document.body.style.paddingTop = navbarHeight + 'px';
 
-    /* Scroll to top of the page on logo click */
-    var links = document.getElementsByClassName("navbar-brand");
-    links[0].onclick = function() {
-      let scroll_top = window.scrollY;
+    window.addEventListener('scroll', navbarAutoHide);
 
-      if (scroll_top > height) {
-        window.scrollTo(0,0);
-      }
+    let brand = document.getElementsByClassName("navbar-brand");
+    brand[0].onclick = brandScrollToTop;
 
-      return false;
-    }
-
-    /* Detect mobile */
-    function isMobile() {
-      try {
-         if(/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)) {
-          return true;
-         };
-         return false;
-      } catch(e){ console.log("Error in isMobile"); return false; }
-    }
-
+    /* Update gallery hover effect on mobile */
     if (isMobile()) {
-      var links = document.querySelectorAll('.img-container');
-      for (var i = 0; i != links.length; ++i) {
-        links[i].onclick = function() {
-          if (this.firstElementChild.tagName == 'A' && this.classList.contains('ready-to-link')) {
-            location = this.firstElementChild.href;
-          } else {
-            ready = document.getElementsByClassName('ready-to-link');
-            if (ready.length) {
-              ready[0].classList.remove('ready-to-link');
-            }
-            this.classList.add('ready-to-link');
-            return false;
-          }
-        }
-      }
+      let work = document.querySelectorAll('.img-container');
+      work.forEach(function(item) {
+        item.onclick = readyToLink;
+      });
     }
 })
